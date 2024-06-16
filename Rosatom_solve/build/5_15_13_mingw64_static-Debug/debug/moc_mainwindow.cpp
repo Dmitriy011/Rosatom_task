77f682c7f092f9de666a5be175ab0652487ab7e0
Dmitriy <rozandima01@mail.ru>
2024-06-16 22:09:55 +0300

    v1.2.

--- Rosatom_solve/build/5_15_13_mingw64_static-Debug/debug/moc_mainwindow.cpp
+++ Rosatom_solve/build/5_15_13_mingw64_static-Debug/debug/moc_mainwindow.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../../../Rosatom_task/Rosatom_solve/mainwindow.h"
+#include "../../../mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
